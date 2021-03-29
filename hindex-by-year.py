2a3408717f21a5ddd1816a21fd9e17e525696e4c
Cristiano Giuffrida <giuffrida@cs.vu.nl>
2021-03-30 00:31:28 +0200

    Add initial implementation

---
+++ hindex-by-year.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+from scholarly import scholarly
+import argparse
+import sys
+
+def computeHindex(pub_cites_per_year, year):
+  cites_per_pub = []
+  for cites_per_year in pub_cites_per_year:
+    count = 0
+    for k,v in cites_per_year.items():
+      if k <= year:
+        count += v
+    cites_per_pub.append(count)
+
+  cites_per_pub.sort(reverse=True)
+  hindex = 0
+  pcount = 0
+  for c in cites_per_pub:
+    pcount += 1
+    if c >= pcount:
+      hindex += 1
+    else:
+      break
+
+  return hindex
+
+# Parse CLI args
+parser = argparse.ArgumentParser(description='Compute h-index of a given author by year')
+parser.add_argument('--from-year', metavar='<year>', type=int, nargs='?', default=0,  help='First year to consider for h-index computation')
+parser.add_argument('--to-year', metavar='<year>', type=int, nargs='?', default=10000, help='Last year to consider for h-index computation')
+parser.add_argument('author', metavar='<author>', type=str, nargs='+',
+                    help='The target author name on Google Scholar')
+args = parser.parse_args()
+firstYear = args.from_year
+lastYear = args.to_year
+author = ' '.join(args.author)
+
+# Query Scholar and index citations per publication per year
+print("Scanning publications for author '%s'..." % author)
+search_query = scholarly.search_author(author)
+authorObject = next(search_query).fill()
+pub_cites_per_year = []
+minYear = 10000
+maxYear = 0
+for pub in authorObject.publications:
+  cites_per_year = pub.fill().cites_per_year
+  pub_cites_per_year.append(cites_per_year)
+  print(" * '%s' (%d)" % (pub.bib['title'], sum(cites_per_year.values())))
+  if 'year' not in pub.bib or pub.bib['year'] == 'NA':
+    continue
+  year = int(pub.bib['year'])
+  if year < minYear:
+    minYear = year
+  if year > maxYear:
+    maxYear = year
+
+# Compute a h-index for each year considered
+if firstYear < minYear:
+  firstYear = minYear
+if lastYear > maxYear:
+  lastYear = maxYear
+print("The h-index of %s at the end of [%d-%d] is:" % (author, firstYear, lastYear))
+for year in range(firstYear, lastYear+1):
+  hindex = computeHindex(pub_cites_per_year, year)
+  print(" %d: %d" % (year, hindex))
